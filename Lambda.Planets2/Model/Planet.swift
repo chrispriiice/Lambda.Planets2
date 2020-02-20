7f63ba18512ef4e9b53edef91a6a74914c1835dc
Chris Price <christopher.r.price@gmail.com>
2020-02-19 22:09:03 -0500

    Set up our files and our basic UI.

--- Lambda.Planets2/Model/Planet.swift
+++ Lambda.Planets2/Model/Planet.swift
@@ -1,9 +1,13 @@
-//
-//  Planet.swift
-//  Lambda.Planets2
-//
-//  Created by Chris Price on 2/19/20.
-//  Copyright © 2020 Chris Price. All rights reserved.
-//
 
-import Foundation
+import UIKit
+
+struct Planet {
+    
+    var name: String
+    var image: UIImage
+    
+    init(name: String, imageName: String) {
+        self.name = name
+        self.image = UIImage(named: imageName)!
+    }
+}
---
+++ Lambda.Planets2/Views/PlanetCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PlanetCollectionViewCell.swift
+//  Lambda.Planets2
+//
+//  Created by Chris Price on 2/19/20.
+//  Copyright © 2020 Chris Price. All rights reserved.
+//
+
+import UIKit
+
+class PlanetCollectionViewCell: UICollectionViewCell {
+    
+}
